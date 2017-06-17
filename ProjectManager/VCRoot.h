52d55b39bcf5877717d413e0e2fe60c61f0c1393
xy <xy@xydemac.local>
2017-06-17 14:46:31 +0800

    add VCRoot

---
+++ ProjectManager/VCRoot.h
@@ -0,0 +1,13 @@
+//
+//  VCRoot.h
+//  ProjectManager
+//
+//  Created by xy on 17/6/17.
+//  Copyright © 2017年 xy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VCRoot : UIViewController
+
+@end
