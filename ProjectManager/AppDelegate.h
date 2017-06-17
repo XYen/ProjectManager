28f8f0c3faf7c2ca883719bb3ac56978780dd782
xy <xy@xydemac.local>
2017-06-17 14:38:45 +0800

    Initial Commit

---
+++ ProjectManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ProjectManager
+//
+//  Created by xy on 17/6/17.
+//  Copyright © 2017年 xy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
